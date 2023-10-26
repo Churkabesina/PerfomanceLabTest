2b87f38fe03af8e43bfcb8780faa317955605917
Zubochistka <ilyacshutov@yandex.ru>
2023-10-26 10:05:25 +0400

    add task4

---
+++ task4/task4.py
@@ -0,0 +1,12 @@
+path = input()
+
+nums = []
+
+with open(path, 'r', encoding='UTF-8') as file_in:
+    for i in file_in:
+        nums.append(int(i.rstrip('\n')))
+
+nums.sort()
+median = nums[len(nums) // 2]
+console_out = sum([abs(x - median) for x in nums])
+print(console_out)
